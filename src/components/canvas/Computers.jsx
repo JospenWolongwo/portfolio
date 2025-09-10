e648eaa4f0ad9c8dbd9d2a83015221a40d4891e5
Jospen Wolongwo <jospenwolongwo@gmail.com>
2025-09-10 19:54:01 +0100

    Fix GLTF routing: Add specific routes for desktop_pc and planet folders

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,17 +1,17 @@
 /* eslint-disable react/no-unknown-property */
 import { Suspense, useEffect, useState } from "react";
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
-import CanvasLoader from '../Loader';
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+import CanvasLoader from "../Loader";
 
 // eslint-disable-next-line react/prop-types
 const Computers = ({ isMobile }) => {
-  const computer = useGLTF('/desktop_pc/scene.gltf');
+  const computer = useGLTF("/desktop_pc/scene.gltf");
   return (
     <mesh>
       <hemisphereLight intensity={Math.PI / 2} groundColor="black" />
       <pointLight intensity={Math.PI} />
-      <spotLight 
+      <spotLight
         decay={0}
         intensity={Math.PI}
         position={[-20, 50, 10]}
@@ -20,10 +20,10 @@ const Computers = ({ isMobile }) => {
         castShadow
         shadow-mapSize={1024}
       />
-      <primitive 
+      <primitive
         object={computer.scene}
-        scale={isMobile ? 0.7 : 0.75} 
-        position={isMobile? [0, -3, -2.2]: [0, -3.25, -1.5]}
+        scale={isMobile ? 0.7 : 0.75}
+        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
@@ -54,23 +54,23 @@ const ComputersCanvas = () => {
     };
   }, []);
 
-
   return (
-    <Canvas 
-      frameLoop='demand'
+    <Canvas
+      frameLoop="demand"
       shadows
       camera={{ position: [20, 3, 5], fov: 25 }}
-      gl={{ preserveDrawingBuffer: true }}>
-        <Suspense fallback={<CanvasLoader />}>
-          <OrbitControls 
-            enableZoom={false}
-            maxPolarAngle={Math.PI / 2}
-            minPolarAngle={Math.PI / 2}
-          />
-          <Computers isMobile={isMobile} />
-        </Suspense>
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Computers isMobile={isMobile} />
+      </Suspense>
 
-        <Preload all />
+      <Preload all />
     </Canvas>
   );
 };
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,23 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  assetsInclude: ['**/*.gltf', '**/*.bin'],
+  build: {
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name && (assetInfo.name.endsWith('.gltf') || assetInfo.name.endsWith('.bin'))) {
+            return 'assets/models/[name][extname]';
+          }
+          return 'assets/[name][extname]';
+        }
+      }
+    }
+  },
+  server: {
+    fs: {
+      allow: ['..']
+    }
+  }
 });
